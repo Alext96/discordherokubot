9179afaf9bf6b81eba2cc9d11332e43e6dc0aff1
Alext96 <alextarberg96@gmail.com>
2019-06-14 11:56:07 +0200

    Add files via upload

---
+++ DiscordHerokuBot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import os
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = ".")
+@client.event
+async def on_ready():
+    print("Thanks for using bot")
+    await client.change_presence(game=discord.Game(name="videos"))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith(".hello"):
+        msg = 'Hello {0.author.mention} How are you today'.format(message)
+        await client.send_message(message.channel, msg)
+    if message.content.startswith(".hello"):
+        msg = 'Good bye {0.author.mention} hope to see u again :wave:'.format(message)
+        await client.send_message(message.channel, msg)
+
+client.run(os.getenv('TOKEN'))
