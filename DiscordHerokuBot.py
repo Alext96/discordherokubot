d864ff36401957e3174fd04a52fe23d966e2b19e
Alext96 <alextarberg96@gmail.com>
2019-06-14 12:11:36 +0200

    Update DiscordHerokuBot.py

--- DiscordHerokuBot.py
+++ DiscordHerokuBot.py
@@ -1,23 +1,13 @@
 import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
-import asyncio
-import time
-import os
-
-Client = discord.Client()
-client = commands.Bot(command_prefix = ".")
+client = discord.Client()
 @client.event
 async def on_ready():
-    print("Thanks for using bot")
-    await client.change_presence(game=discord.Game(name="Baby Seal"))
-
+    print("The bot is ready!")
+    await client.change_presence(game=discord.Game(name="Making a    bot"))
 @client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
     if message.content == "Hello":
         await client.send_message(message.channel, "World")
-    if message.content.startswith(".hello"):
-        msg = 'Good bye {0.author.mention} hope to see u again :wave:'.format(message)
-        await client.send_message(message.channel, msg)
-
-client.run(os.getenv('TOKEN'))
+client.run(TOKEN)
